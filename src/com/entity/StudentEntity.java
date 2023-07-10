e05532643aeda011c135ef568327feca227e0daf
KKchan1119 <kkchan1119@gmail.com>
2023-07-10 10:49:46 +0900

    default set

--- src/com/entity/StudentEntity.java
+++ src/com/entity/StudentEntity.java
@@ -8,6 +8,8 @@ public class StudentEntity {
 	//신규 데이터 저장시 배열의 위치정보
 	int nextArrIdx;
 	
+	int stuArrIdx;
+	int stuNextNo;
 	//현재 DB의 가용 공간 상태
 	boolean stuArrStatus = false;
 	
@@ -23,17 +25,40 @@ public class StudentEntity {
 	public static int ERR_ARRAY_FULL = 2;
 	
 	// StudentEntity 초기화, 객체 배열 생성.
-	
+	public int initStudentEntity(int arrLength, boolean sampleDataInsert) {
+		//매개변수로 받은 길이로 배열 생성
+		stuArr = new Student[arrLength];
+		
+		//샘플 데이터 입력 플래그 확인
+		if(sampleDataInsert && arrLength > StudentEntity.SAMPLE_DATA_COUNT) {
+			//샘플 데이터 입력
+			for(int i = 0; i < StudentEntity.SAMPLE_DATA_COUNT; i++) {
+				
+				//Student 객체 생성.
+				Student student = new Student();
+			}
+		}
+		//샘플 데이터 입력
+		
+		return 0;
+	}
 	// 한 건의 데이터가 저장되는 기능.
 	public int insertStudent(Student stu) {
 		//DB에 저장 가능 한지 확인
-		
+		if(stuArrStatus) return ERR_ARRAY_FULL;
 		//배열에 학생정보 저장.
-		
+		stu.setStuNo(stuNextNo);
+		stu.setStuName(stu.getStuName() + stuNextNo);
+		stuArr[nextArrIdx] = stu;
 		//다음 데이터를 위해 DB 관리정보 갱신.
-		
+		stuNextNo += 1;
+		stuArrIdx += 1;
 		//다음 데이터를 DB에 저장할 수 있는 상태인지 체크.
+		if(stuArr.length == nextArrIdx)
+			stuArrStatus = StudentEntity.ARRAY_STATUS_FULL;
+		else
+			stuArrStatus = StudentEntity.ARRAY_STATUS_NO_FULL;
 		
-		return 0;
+		return nextArrIdx;
 	}
 }
